8edd1326d27cb3310db19ceb86adff762c08904e
bjackson312006 <blakejackson312006@gmail.com>
2026-06-16 11:56:18 -0700

    fix failing checks (adding ::Can to the blocks in the JSONs)

--- nxp-pac/src/chips/mcxa256/metadata.rs
+++ nxp-pac/src/chips/mcxa256/metadata.rs
@@ -1066,7 +1066,7 @@ pub const PERIPHERALS: &[Peripheral] = &[
     Peripheral {
         name: "CAN0",
         address: 0x400CC000,
-        driver_name: "mcxa/CAN2xx",
+        driver_name: "mcxa/CAN2xx::Can",
         signals: &[
             Signal {
                 name: "RXD",
@@ -1132,7 +1132,7 @@ pub const PERIPHERALS: &[Peripheral] = &[
     Peripheral {
         name: "CAN1",
         address: 0x400D0000,
-        driver_name: "mcxa/CAN2xx",
+        driver_name: "mcxa/CAN2xx::Can",
         signals: &[
             Signal {
                 name: "RXD",
